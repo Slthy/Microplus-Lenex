7b22cd5fe64c7007e3ebcba872d584d71f2779c6
Alessandro Borsato <alessandroborsato5@gmail.com>
2022-12-30 23:34:04 +0100

    build_lenex 1/2 (JSON)

--- functions.py
+++ functions.py
@@ -3,92 +3,11 @@ import json
 import pathlib
 import os
 import datetime
+import utils
+#TODO: #1 setup python env, (do in at the end)
 
-RACE_CODES = {
-    "50m Freestyle": "001",
-    "100m Freestyle": "002",
-    "200m Freestyle": "003",
-    "400m Freestyle": "004",
-    "800m Freestyle": "005",
-    "1000m Freestyle": "006",
-    "1500m Freestyle": "007",
-    "3000m Freestyle": "008",
-    "5000m Freestyle": "009",
-    "50m Backstroke": "011",
-    "100m Backstroke": "012",
-    "200m Backstroke": "013",
-    "50m Breaststroke": "016",
-    "100m Breaststroke": "017",
-    "200m Breaststroke": "018",
-    "50m Butterfly": "026",
-    "100m Butterfly": "027",
-    "200m Butterfly": "028",
-    "100m Individual Medley": "032",
-    "200m Individual Medley": "033",
-    "400m Individual Medley": "034",
-    "4 x 50 m Freestyle": "041",
-    "4 x 100m Freestyle": "042",
-    "4 x 200m Freestyle": "043",
-    "4 x 100 m Medley": "045"
-}
-
-RACE_TYPES = {
-    "001": {
-        'microplus': 'preliminary',
-        'lenex': 'PRE'
-    },
-    "002": {
-        'microplus': 'spareggio',
-        'lenex': 'SOP'
-    },
-    "003": {
-        'microplus': 'semifinals',
-        'lenex': 'SEM'
-    },
-    "004": {
-        'microplus': 'spareggio2',
-        'lenex': 'SOS'
-    },
-    "005": {
-        'microplus': 'final',
-        'lenex': 'FIN'
-    },
-    "006": {
-        'microplus': 'young final',
-        'lenex': 'FIN'
-    },
-    "007": {
-        'microplus': 'heats',
-        'lenex': 'TIM'
-    }
-}
-
-FILE_TYPES = {
-    'SCH_D': 'schedules/by_date',
-    'SCH_E': 'schedules/by_event',
-    'STL1': 'startlists',
-    'SUM':  'summary',
-    'CGR1': 'results',
-    'other': 'other'
-}
-
-LENEX_STROKES = {
-    'Freestyle': 'FREE',
-    'Butterfly': 'FLY',
-    'Breaststroke': 'BREAST',
-    'Backstroke': 'BACK',
-    'Individual Medley': 'MEDLEY',
-    'Medley': 'MEDLEY'
-}
-
-
-def url_util(url: str) -> list:
-    output = url.split('NU_')
-    return [output[0]+'export/NU_', output[1].replace('_web.php', '')]
-
-
-def scrape_data(url: str):
-    url_elab: list = url_util(url)
+def scrape_data(url: str) -> None:
+    url_elab: list = utils.url(url)
     base_url = url_elab[0]
     event = url_elab[1]
     counter_generale = requests.get(
@@ -102,14 +21,15 @@ def scrape_data(url: str):
             f'{base_url}{event}/NU/{obj["nomefile"]}?x={obj["counter"]}').json()
 
         # assigns a category to the downloaded json. This category will also be part of its filepath
-        file_type = obj['cod'] if obj['cod'] in FILE_TYPES.keys() else 'other'
+        file_type = obj['cod'] if obj['cod'] in utils.FILE_TYPES.keys(
+        ) else 'other'
 
         # create directory for a new type of file
         pathlib.Path(
-            f'scraped_data/{FILE_TYPES[file_type]}').mkdir(parents=True, exist_ok=True)
+            f'scraped_data/{utils.FILE_TYPES[file_type]}').mkdir(parents=True, exist_ok=True)
 
         # write file into category path
-        with open(f'scraped_data/{FILE_TYPES[file_type]}/{scraped_data["jsonfilename"]}', 'w') as f:
+        with open(f'scraped_data/{utils.FILE_TYPES[file_type]}/{scraped_data["jsonfilename"]}', 'w') as f:
             f.write(json.dumps(scraped_data))
             f.close()
 
@@ -117,45 +37,109 @@ def scrape_data(url: str):
 def get_competition_infos() -> dict:
 
     # reads the first 'result' in the 'results' folder, retrieves generic data and asks to the user the missing infos
-    with open(os.listdir('scraped_data/results')[0], 'r') as f:
+    with open(f"scraped_data/results/{os.listdir('scraped_data/results')[0]}", 'r') as f:
         data = json.loads(f.read())
-        return {  # this script is specifically designed to scrape data from Microplus' systems #TODO: maybe put these infos directly in the xml
-            'constructor': {
-                'name': 'Microplus Informatica Srl',
-                'zip': 'IT-12030',
-                'city': 'Marene',
-                'email': 'mbox@microplus.it',
-                'internet': 'https://www.microplus.it'
+        pool_length_code = input('insert course lenght (LCM or SCM): ')
+        return {
+            'data': {  # this script is specifically designed to scrape data from Microplus' systems #TODO: maybe put these infos directly in the xml
+                'constructor': {
+                    'name': 'Microplus Informatica Srl',
+                    'zip': 'IT-12030',
+                    'city': 'Marene',
+                    'email': 'mbox@microplus.it',
+                    'internet': 'https://www.microplus.it'
+                },
+                'event': {  # generic data about the competition's venue
+                    'name': data['Export']['ExpName'],
+                    'desciption': data['Export']['ExpDescr'],
+                    'city': data['Event']['Place'].split(',')[0],
+                    'nation': 'ITA' if data['Event']['Place'].split(',')[0] == 'Roma'
+                    else input(f'insert nation (city: {data["Event"]["Place"].split(",")[0]}): '),
+                    'course': pool_length_code,  # TODO: #2 check?, inquirer module
+                    'timing': "AUTOMATIC",
+                    'pool_lane_min': '0',
+                    'pool_lane_max': '9'
+                },
             },
-            'event': {  # generic data about the competition's venue
-                'name': data['Export']['ExpName'],
-                'desciption': data['Export']['ExpDescr'],
-                'city': data['Event']['Place'].split(',')[0],
-                'nation': 'ITA' if data['Event']['Place'].split(',')[0] == 'Roma'
-                else input(f'insert nation (city: {data["Event"]["Place"].split(",")[0]}): '),
-                'course': input('insert course lenght (LCM or SCM): '),
-                'timing': "AUTOMATIC",
-                'pool_lane_min': '0',
-                'pool_lane_max': '10'
-            }
+            'pool_length': 50 if pool_length_code == 'LCM' else 25
         }
 
 
-def get_heats(event: dict, eventid: int) -> dict:
-    with open(f'scraped_data/results/NU{event["c0"]}{RACE_CODES[event["d_en"]]}CLAS{event["c2"][-2::]} 001.JSON', 'r') as f:
-        heat_entries: list = json.loads(f.read())['data']
+def get_entry_time(category: str, race_code, event_type: str, PlaCod: str) -> str: # returns the entry time in an event for a given athlete
+    with open(f'scraped_data/startlists/NU{category}{utils.utils.RACE_CODES[race_code]}STAR{event_type} 001.JSON', 'r') as f:
+        for entry in json.loads(f.read())['data']:
+            if entry['PlaCod'] == PlaCod:
+                return utils.format_time(entry['MemIscr'])
+
+
+def get_heats(event: dict, eventid: int, pool_length: int) -> dict: # returns LENEX 'heats' component for the given event and all the athlete entries 
+    with open(f'scraped_data/results/NU{event["c0"]}{utils.RACE_CODES[event["d_en"]]}CLAS{event["c2"][::2]} 001.JSON', 'r') as f:
+        heat_entries: list = json.loads(f.read())
+        date = heat_entries['Heat']['UffDate']
+        data: list = heat_entries['data']
+        athlete_entries = []
         heats = {}
         heat_n = 1
-        for entry in heat_entries:
+        for entry in data:
+            entrytime = get_entry_time(
+                event["c0"], event["d_en"], event["c2"][::2], entry['PlaCod'])
+            heatid = f'{heat_n}{"0"*(5-(len(str(heat_n)) + len(str(eventid))))}{eventid}' # 'heatid' is composed of the heat's at the head, eventid at the tail, filled in between by '0's
+            swimtime = utils.format_time(entry['MemPrest'])
+            splits = []
+            for index, time in enumerate(entry['MemFields'][1:]): #first element is blank every time, so we cut it
+                if time['V'] == "":
+                    break
+
+                splits.append({
+                    'distance': str(pool_length*index + pool_length),
+                    'swimtime': utils.format_time(time['V'])
+                })
+
+            athlete_entries.append({
+                'athlete_infos': {
+                    # TODO: #5 swimrankings codes if possible (and needed)
+                    'athleteid': entry['PlaCod'],
+                    'lastname': entry['PlaSurname'],
+                    'firstname': entry['PlaName'],
+                    'gender': event["c0"][-1],
+                    'birthdate': entry['PlaBirth'],
+                    'team': {
+                        'name': entry['TeamDescrIta'],
+                        'shortname': entry['TeamDescrItaVis'],
+                        'code': entry['PlaNat'],
+                        'nation': entry['PlaNat'],
+                        'type': 'CLUB'  # hardcoded
+                    }
+                },
+                'entry': {
+                    'eventid': eventid,
+                    'entrytime': entrytime,
+                    'heat': entry['b'],
+                    'lane': entry['PlaLane'],
+                    'meetinfo': date
+                },
+                'result': {
+                    'eventid': eventid,
+                    'place': entry['PlaCls'],
+                    'lane': entry['PlaLane'],
+                    'heat': heat_n,
+                    'heatid': heatid,
+                    'swimtime': swimtime,
+                    'reactiontime': '',  # TODO: #6 check if value is given in other microplus events
+                    'splits': splits
+                }
+            })
             if entry['b'] not in heats.keys():
                 heats[entry['b']] = [{
+                    # TODO: #7 check for possible bug regarding this field, sometimes is empty
                     'daytime': event['h'],
-                    'heatid': f'{heat_n}{"0"*(5-(len(str(heat_n)) + len(str(eventid))))}{eventid}', # heatid is by default 5 char long, composed by the heat's number at the start and event's id at the end, in the middle '0's fill the remaining chars
+                    # heatid is by default 5 char long, composed by the heat's number at the start and event's id at the end, in the middle '0's fill the remaining chars
+                    'heatid': heatid,
                     'number': str(heat_n)
                 }]
                 heat_n = heat_n + 1
         f.close()
-    return {'heats': dict(sorted(heats.items()))}
+    return {'heats': dict(sorted(heats.items())), 'entries': athlete_entries}
 
 
 def get_event_infos(event: dict, eventid: int, filename: str) -> dict:
@@ -164,50 +148,57 @@ def get_event_infos(event: dict, eventid: int, filename: str) -> dict:
         'session': int(filename[15:-5:]),
         'name': event["d_en"],
         'category': event['c0'],
-        'race_code': RACE_CODES[event["d_en"]],
+        'race_code': utils.RACE_CODES[event["d_en"]],
         'race_type': event['c2'],
         # results filename
-        'jsonfilename': f'NU{event["c0"]}{RACE_CODES[event["d_en"]]}CLAS{event["c2"][-2::]} 001.JSON',
+        'jsonfilename': f'NU{event["c0"]}{utils.RACE_CODES[event["d_en"]]}CLAS{event["c2"][-2::]} 001.JSON',
         'lenex': {
             'event': {
                 'eventid': str(eventid),
-                'number': RACE_CODES[event["d_en"]],
+                'number': utils.RACE_CODES[event["d_en"]],
                 # '-1' means that the event is a preliminary or heat, so there isn't "parent" event for the current event.
                 'preveventid': '-1' if (event['c2'] == '001' or event['c2'] == '007') else '00',
                 'gender': event['c0'][-1::],
-                'round': RACE_TYPES[event["c2"]]['lenex'],
-                'daytime': event['h']
+                'round': utils.RACE_TYPES[event["c2"]]['lenex'],
+                'daytime': event['h'],
+                'pool': {
+                    'lanemax': '8' if utils.RACE_TYPES[event["c2"]]['microplus'] == '007' or '006' else '9',
+                    'lanemin': '1' if utils.RACE_TYPES[event["c2"]]['microplus'] == '007' or '006' else '0'
+                }
             },
             'swimstyle': {
                 'distance': swimstyle_split[0].strip() if 'x' not in swimstyle_split[0] else swimstyle_split[0].strip().replace('4 x ', ''),
                 'relaycount': '4' if 'x' in swimstyle_split[0].strip() else '1',
-                'stroke':  LENEX_STROKES[swimstyle_split[1].strip()]
+                'stroke':  utils.LENEX_STROKES[swimstyle_split[1].strip()]
             }
         }
     }
 
 
-def get_sessions() -> dict:
+def convert_to_lenex(pool_length: int) -> dict:
 
     events = []
     prelims_eventid = []
     eventid = 1
+    athletes_entries = []
 
     # create directory to store the processed data
     pathlib.Path('processed_data').mkdir(parents=True, exist_ok=True)
     for filename in os.listdir('scraped_data/schedules/by_date'):
         file = os.path.join('scraped_data/schedules/by_date', filename)
+
         if os.path.isfile(file):
             with open(file, 'r') as f:
                 data: list = json.loads(f.read())['e']
                 for event in data:
                     infos = get_event_infos(event, eventid, filename)
-                    heats = get_heats(event, eventid)
-                    race = infos | heats
+                    heats_data = get_heats(event, eventid, pool_length)
+                    race = infos | heats_data['heats']
+                    athletes_entries = athletes_entries + heats_data['entries']
                     # if the event is a preliminary or heat, put race_code, eventid and -current event's- category into the prelims list
                     if race['lenex']['event']['preveventid'] == '-1':
                         prelims_eventid.append({
-                            'race_code': RACE_CODES[event["d_en"]],
+                            'race_code': utils.RACE_CODES[event["d_en"]],
                             'eventid': race['lenex']['event']['eventid'],
                             'category': event['c0']
                         })
@@ -216,7 +207,7 @@ def get_sessions() -> dict:
                 f.close()
 
     for race in events:
-        # if event has a prev_event, the parent event in the prelims list. This script is designed for 'normal' event, no semis. # TODO: handle semis (and quarters)
+        # if event has a prev_event, the parent event in the prelims list. This script is designed for 'normal' event, no semis. # TODO: #8 handle semis (and quarters)
         if race['lenex']['event']['preveventid'] == '00':
             for prelim in prelims_eventid:
                 if prelim['race_code'] == race['race_code'] and prelim['category'] == race['category']:
@@ -241,9 +232,47 @@ def get_sessions() -> dict:
                 },
                 'events': sessions[key]
             }
+            f.close()
 
-    with open('processed_data/sessions.json', 'w') as f:  # write results
-        f.write(json.dumps(sessions))
+    clubs = {}
+    for entry in athletes_entries:
+        club_name = entry['athlete_infos']['team']['name']
+        athleteid = entry['athlete_infos']['athleteid']
+
+        if club_name not in clubs.keys(): # new club 
+            club_infos = entry['athlete_infos']['team']
+            del entry['athlete_infos']['team']
+
+            clubs[club_name] = {
+                'infos': club_infos,
+                'athletes': {
+                    athleteid: {
+                        'athlete_infos': entry['athlete_infos'],
+                        'entries': [entry['entry']],
+                        'results': [entry['result']]
+                    }
+                }
+            }
+        elif athleteid not in clubs[club_name]['athletes'].keys(): # club already in 'clubs' dict, new athlete to add
+            del entry['athlete_infos']['team']
+
+            clubs[club_name]['athletes'][athleteid] = {
+                'athlete_infos': entry['athlete_infos'],
+                'entries': [entry['entry']],
+                'results': [entry['result']]
+            }
+        else: # both club and athlete are in the respective dicts, append new data to 'entries' and 'results' field
+            clubs[club_name]['athletes'][athleteid]['entries'].append(
+                entry['entry'])
+            clubs[club_name]['athletes'][athleteid]['results'].append(
+                entry['result'])
+
+    return {'sessions': sessions, 'clubs': clubs}
+
+
+def build_lenex() -> None:
+    competition_infos: dict = get_competition_infos()
+    converted_data = convert_to_lenex(competition_infos['pool_length'])
+    with open('processed_data/lenex_data.json', 'w') as f:  # write results
+        f.write(json.dumps(competition_infos['data'] | converted_data))
         f.close()
-
-    return sessions
---
+++ utils.py
@@ -0,0 +1,101 @@
+import requests
+import datetime
+from bs4 import BeautifulSoup
+
+RACE_CODES = {
+    "50m Freestyle": "001",
+    "100m Freestyle": "002",
+    "200m Freestyle": "003",
+    "400m Freestyle": "004",
+    "800m Freestyle": "005",
+    "1000m Freestyle": "006",
+    "1500m Freestyle": "007",
+    "3000m Freestyle": "008",
+    "5000m Freestyle": "009",
+    "50m Backstroke": "011",
+    "100m Backstroke": "012",
+    "200m Backstroke": "013",
+    "50m Breaststroke": "016",
+    "100m Breaststroke": "017",
+    "200m Breaststroke": "018",
+    "50m Butterfly": "026",
+    "100m Butterfly": "027",
+    "200m Butterfly": "028",
+    "100m Individual Medley": "032",
+    "200m Individual Medley": "033",
+    "400m Individual Medley": "034",
+    "4 x 50 m Freestyle": "041",
+    "4 x 100m Freestyle": "042",
+    "4 x 200m Freestyle": "043",
+    "4 x 100 m Medley": "045"
+}
+
+RACE_TYPES = {
+    "001": {
+        'microplus': 'preliminary',
+        'lenex': 'PRE'
+    },
+    "002": {
+        'microplus': 'spareggio',
+        'lenex': 'SOP'
+    },
+    "003": {
+        'microplus': 'semifinals',
+        'lenex': 'SEM'
+    },
+    "004": {
+        'microplus': 'spareggio2',
+        'lenex': 'SOS'
+    },
+    "005": {
+        'microplus': 'final',
+        'lenex': 'FIN'
+    },
+    "006": {
+        'microplus': 'young final',
+        'lenex': 'FIN'
+    },
+    "007": {
+        'microplus': 'heats',
+        'lenex': 'TIM'
+    }
+}
+
+FILE_TYPES = {
+    'SCH_D': 'schedules/by_date',
+    'SCH_E': 'schedules/by_event',
+    'STL1': 'startlists',
+    'SUM':  'summary',
+    'CGR1': 'results',
+    'other': 'other'
+}
+
+LENEX_STROKES = {
+    'Freestyle': 'FREE',
+    'Butterfly': 'FLY',
+    'Breaststroke': 'BREAST',
+    'Backstroke': 'BACK',
+    'Individual Medley': 'MEDLEY',
+    'Medley': 'MEDLEY'
+}
+
+
+def url(url: str) -> list:
+    output = url.split('NU_')
+    return [output[0]+'export/NU_', output[1].replace('_web.php', '')]
+
+
+def swrid(lastname: str, firstname: str): #query-search athlete through swimrakings.net, returns its swrid (swimrakings id)
+    html_res = requests.get(
+        f'https://www.swimrankings.net/index.php?&internalRequest=athleteFind&athlete_clubId=-1&athlete_gender=-1&athlete_lastname={lastname}&athlete_firstname={firstname}')
+    return BeautifulSoup(html_res.text, features="html.parser").find('a', href=True)['href'].replace('?page=athleteDetail&athleteId=', '')
+
+
+def format_time(time: str) -> str:
+    if len(time) > 4:  # valid entrytime
+        if len(time) > 5:  # entrytime with minutes
+            return datetime.datetime.strptime(time, "%M'%S.%f").strftime("%H:%M:%S.%f")[:-4]
+        else:
+            return datetime.datetime.strptime(time, "%S.%f").strftime("%H:%M:%S.%f")[:-4]
+    else:  # dnf, ...
+        return 'NT'
